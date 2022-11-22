ec8f6b5c8d9ae095cd2ba347f436015fc3a5a85b
Rustlanachain <hamza.sajid@invozone.com>
2022-11-23 00:44:32 +0500

    imported all utilities

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+extern crate flate2;
+
+use flate2::write::GzEncoder;
+use flate2::Compression;
+use std::env::args;
+use std::fs::File;
+use std::io::copy;
+use std::io::BufReader;
+use std::time::Instant;
+
+fn main(){
+    if args().len() != 3{
+        eprintln!("Usage: 'source' 'target'")
+    }
+    let mut input = BufReader::new(File::open(args().nth(1).unwrap()).unwrap());
 }
